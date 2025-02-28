93418e486d1ad01e52b9a9f0a58d4bebc976eb76
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2025-02-28 13:06:33 -0300

    Add getters and setters

--- src/models/Bank.java
+++ src/models/Bank.java
@@ -2,4 +2,12 @@ package src.models;
 
 public class Bank {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/models/Client.java
+++ src/models/Client.java
@@ -1,5 +1,13 @@
 package src.models;
 
 public class Client {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String name;
 }
