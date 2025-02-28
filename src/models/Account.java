dbbc8f0803cd45ce249971892faef8f842d3c83a
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2025-02-28 13:14:28 -0300

    Add super class Account

---
+++ src/models/Account.java
@@ -0,0 +1,23 @@
+package src.models;
+
+public class Account {
+    private int agency;
+    private int id;
+    private double balance;
+
+    public int getAgency() {
+        return agency;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(){}
+    public void withdraw(){}
+    public void transfer(){}
+}
