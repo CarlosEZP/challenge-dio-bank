a21a279124738fe3f6bca8d3967206c0f47449c8
Carlos EZ Pereira <carloszanca03@gmail.com>
2025-03-01 11:28:12 -0300

    :sparkles: feat: Add Account constructor.

--- src/models/Account.java
+++ src/models/Account.java
@@ -1,9 +1,14 @@
 package src.models;
-
-public class Account {
+public abstract class Account {
+    private static int STANDARD_AGENCY = 1;
+    private static int ID_SEQUENCIAL = 1;
     protected int agency;
     protected int id;
     protected double balance;
+    public Account(){
+        this.agency = STANDARD_AGENCY;
+        this.id = ID_SEQUENCIAL++;
+    }
 
     public int getAgency() {
         return agency;
